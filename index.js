4406b65ec95f6eb0286784b85e9c115092a6212b
Jordan Pickwell <jpickwell@neuone.com>
2018-05-16 08:29:36 -0500

    Initial rewrite.

--- index.js
+++ index.js
@@ -1,26 +1,104 @@
-var path = require('path');
-var findParentDir = require('find-parent-dir');
-var fs = require('fs');
+var path
 
-function resolve(targetUrl, source) {
-  var packageRoot = findParentDir.sync(source, 'node_modules');
+path = require('path')
 
-  if (!packageRoot) {
-    return null;
-  }
-
-  var filePath = path.resolve(packageRoot, 'node_modules', targetUrl);
-  var isPotentiallyDirectory = !path.extname(filePath);
-
-  if (isPotentiallyDirectory && fs.existsSync(filePath)) {
-    return path.resolve(filePath, 'index');
-  } else if (fs.existsSync(path.dirname(filePath))) {
-    return filePath;
-  }
-
-  return resolve(targetUrl, path.dirname(packageRoot));
+function resolve (dir, importToResolve) {
+  return new Promise(function (pResolve, pReject) {
+    try {
+      pResolve(require.resolve(importToResolve, { paths: [dir] }))
+    } catch (ex) {
+      pReject(ex)
+    }
+  })
 }
 
-module.exports = function importer (url, prev, done) {
-  return (url[ 0 ] === '~') ? { file: resolve(url.substr(1), prev) } : null;
-};
+// Modified from https://github.com/webpack-contrib/sass-loader/blob/master/lib/webpackImporter.js#L44-L60
+function startResolving (dir, imports) {
+  if (imports.length === 0) {
+    return Promise.reject()
+  }
+
+  return resolve(dir, imports[0])
+    .then(
+      function (resolvedFile) {
+        // By removing the CSS file extension, we trigger node-sass to include the CSS file instead of just linking it.
+        return { file: resolvedFile.replace(/\.css$/, '') }
+      },
+      function () {
+        return startResolving(dir, imports.slice(1))
+      }
+    )
+}
+
+// Modified from https://github.com/webpack-contrib/sass-loader/blob/master/lib/importsToResolve.js
+function importsToResolve (url) {
+  var baseName, dirName, ext, imports, request
+
+  request = url.substr(1)
+
+  // Keep in mind: ext can also be something like '.datepicker' when the true extension is omitted and the filename
+  // contains a dot.
+  ext = path.extname(url)
+
+  // Test if the url is a node package main import.
+  if (/^~([^\/]+|@[^\/]+[\/][^\/]+)$/g.test(url)) {
+    return [request, url]
+  }
+
+  // libsass' import algorithm works like this:
+
+  // In case there is a file extension...
+  // - If the file is a CSS-file, do not include it all, but just link it via @import url().
+  // - The exact file name must match (no auto-resolving of '_'-modules).
+  if (ext === '.css') {
+    return []
+  }
+
+  if (ext === '.scss' || ext === '.sass') {
+    return [request, url]
+  }
+
+  imports = [
+    request + '.scss',
+    request + '.sass',
+    request + '.css',
+    url
+  ]
+
+  // In case there is no file extension...
+  // - Prefer modules starting with '_'.
+  // - File extension precedence: .scss, .sass, .css.
+  baseName = path.basename(request)
+  if (baseName.charAt(0) === '_') {
+    return imports
+  }
+
+  dirName = path.dirName(request)
+
+  imports.unshift(
+    dirName + '/_' + baseName + '.scss',
+    dirName + '/_' + baseName + '.sass',
+    dirName + '/_' + baseName + '.css'
+  )
+
+  return imports
+}
+
+// Modified from https://github.com/webpack-contrib/sass-loader/blob/master/lib/webpackImporter.js#L62-L69
+function importer (url, prev, done) {
+  if (url[0] !== '~') {
+    return null
+  }
+
+  startResolving(
+    path.dirname(prev),
+    importsToResolve(url)
+  )
+    // Catch all resolving errors, return the original file and pass responsibility back to other custom importers.
+    .catch(function () {
+      return { file: url }
+    })
+    .then(done)
+}
+
+module.exports = importer
--- index.test.js
+++ index.test.js
@@ -1,80 +1,99 @@
-jest.mock('find-parent-dir');
-jest.mock('fs');
+var importer
 
-var importer = require('./');
-var mockFs = require('fs');
-var mockFindParentDir = require('find-parent-dir');
 
-describe('Importer', function() {
-  beforeEach(function() {
-    mockFs.existsSync.mockClear();
-    mockFindParentDir.sync.mockReturnValue('MOCK_PARENT_DIR').mockClear();
+
+importer = require('./')
+
+function importerExpect (url, prev, expectation, done) {
+  var res
+
+  function _done (result) {
+    expect(result).toEqual(expectation)
+    done()
+  }
+
+  res = importer(url, prev, _done)
+
+  if (typeof res !== 'undefined') {
+    _done(res)
+  }
+}
+
+describe('Importer', function () {
+  beforeEach(function () {
+    // mockFs.existsSync.mockClear()
+    // mockFindParentDir.sync.mockReturnValue('MOCK_PARENT_DIR').mockClear()
   });
 
-  test('resolves to node_modules directory when first character is ~', function() {
-    mockFs.existsSync.mockReturnValue(true);
-    expect(importer('~my-module', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/index'
-    });
-  });
+  test('resolves to node_modules directory when first character is ~', function (done) {
+    // mockFs.existsSync.mockReturnValue(true)
 
-  test('does nothing when the first character isnt a ~', function() {
-    expect(importer('my-module', '')).toEqual(null);
-  });
+    importerExpect('~my-module', '', { file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/index' }, done)
+  })
 
-  test('recursively resolve url until package has not been found', function() {
-    var mockFsCheck = mockFs.existsSync,
-    mockParentDirFinder = mockFindParentDir.sync;
+  test("does nothing when the first character isn't a ~", function (done) {
+    importerExpect('my-module', '', null, done)
+  })
+
+  test('recursively resolve url until package has not been found', function (done) {
+    // var i, mockFsCheck, mockParentDirFinder
+
+    // mockFsCheck = mockFs.existsSync
+    // mockParentDirFinder = mockFindParentDir.sync
 
     // url can not be resolved up to 10 level
-    for (var i = 0; i < 10; i++) {
-      mockFsCheck = mockFsCheck
-        .mockReturnValueOnce(false) // directory import
-        .mockReturnValueOnce(false); // file import
-
-      mockParentDirFinder = mockParentDirFinder.mockReturnValueOnce('MOCK_PARENT_DIR' + i);
-    }
+    // for (i = 0; i < 10; ++i) {
+    //   mockFsCheck = mockFsCheck
+    //     .mockReturnValueOnce(false) // directory import
+    //     .mockReturnValueOnce(false) // file import
+    //
+    //   mockParentDirFinder = mockParentDirFinder.mockReturnValueOnce('MOCK_PARENT_DIR' + i)
+    // }
 
     // url finally found
-    mockFsCheck = mockFsCheck.mockReturnValueOnce(false).mockReturnValueOnce(true);
-    mockParentDirFinder = mockParentDirFinder.mockReturnValueOnce('MOCK_PARENT_DIR_final');
+    // mockFsCheck = mockFsCheck.mockReturnValueOnce(false).mockReturnValueOnce(true)
+    // mockParentDirFinder = mockParentDirFinder.mockReturnValueOnce('MOCK_PARENT_DIR_final')
 
-    expect(importer('~my-module/test', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR_final/node_modules/my-module/test'
-    });
+    importerExpect(
+      '~my-module/test', '', { file: __dirname + '/MOCK_PARENT_DIR_final/node_modules/my-module/test' },
+      function () {
+        // expect(mockParentDirFinder.mock.calls.length).toBe(11)
+        // expect(mockFsCheck.mock.calls.length).toBe(22)
+        done()
+      }
+    )
+  })
 
-    expect(mockParentDirFinder.mock.calls.length).toBe(11);
-    expect(mockFsCheck.mock.calls.length).toBe(22);
-  });
+  test('return original url when package file can not be resolved', function (done) {
+    var url
+    // mockFindParentDir.sync.mockReturnValue(null)
 
-  test('return null when package file can not be resolved', function() {
-    mockFindParentDir.sync.mockReturnValue(null);
-    expect(importer('~my-module', '')).toEqual({ file: null });
-  });
+    url = '~my-module'
 
-  test('should resolve extensions', function() {
-    mockFs.existsSync.mockReturnValueOnce(true);
+    importerExpect(url, '', { file: url }, done)
+  })
 
-    expect(importer('~my-module/test.scss', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/test.scss'
-    });
-  });
+  test('should resolve extensions', function (done) {
+    // mockFs.existsSync.mockReturnValueOnce(true)
 
-  test('should support file imports', function() {
-    mockFs.existsSync
-      .mockReturnValueOnce(false) // directory import
-      .mockReturnValueOnce(true); // file import
+    importerExpect(
+      '~my-module/test.scss', '', { file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/test.scss' }, done
+    )
+  })
 
-    expect(importer('~my-module/test', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/test'
-    });
-  });
+  test('should support file imports', function (done) {
+    // mockFs.existsSync
+    //   .mockReturnValueOnce(false) // directory import
+    //   .mockReturnValueOnce(true) // file import
 
-  test('should support directory imports', function() {
-    mockFs.existsSync.mockReturnValueOnce(true); // directory import
+    importerExpect('~my-module/test', '', { file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/test' }, done)
+  })
 
-    expect(importer('~my-module/test', '')).toEqual({
-      file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/test/index'
-    });
-  });
-});
+  test('should support directory imports', function (done) {
+    // mockFs.existsSync.mockReturnValueOnce(true) // directory import
+
+    importerExpect(
+      '~my-module/test', '', { file: __dirname + '/MOCK_PARENT_DIR/node_modules/my-module/test/index' }, done
+    )
+  })
+})
